f54e81216cb41d261a4e415f1ed5898095d1db5f
jthomasbarry <jordan.t.barry@gmail.com>
2016-01-18 15:15:11 -0600

    issue fixes

---
+++ LaTeXtoLaTeX-master/myoperations.py
@@ -0,0 +1,43 @@
+
+import re
+
+import utilities
+import component
+
+def setvariables(text):
+
+    component.chapter_abbrev = utilities.argument_of_macro(text,"chap",2)
+
+
+###################
+
+def mytransform(text):
+
+    thetext = text
+
+    # replace \begin{prop}{the_label} by
+    # \begin{prop}\label{proposition:chaptername:the_label}
+    thetext = utilities.replacemacro(thetext,r"\begin{prop}",1,
+                         r"\begin{prop}\label{proposition:"+component.chapter_abbrev+":#1}")
+
+    # and similarly for example and exercise (yes, this can be in a loop)
+    thetext = utilities.replacemacro(thetext,r"\begin{example}",1,
+                         r"\begin{example}\label{example:"+component.chapter_abbrev+":#1}")
+    thetext = utilities.replacemacro(thetext,r"\begin{exercise}",1,
+                         r"\begin{exercise}\label{exercise:"+component.chapter_abbrev+":#1}")
+
+    # in actions.tex and crypt.tex many examples start with something like
+    # \noindent {\bf Example 2.}
+    # and end with
+    # \hspace{\fill} $\blacksquare$
+    # so we convert these to \begin{example} \end{example}.
+    # Labels and references still need to be added by hand.
+
+    thetext = re.sub(r"\\noindent\s*{\\bf\s+Example\s+[0-9.]+\s*}",r"\\begin{example}",thetext)
+    thetext = re.sub(r"\\hspace{\\fill}\s*\$\\blacksquare\$",r"\\end{example}",thetext)
+
+    # delete empty label arguments
+    thetext = re.sub(r"\\label{[a-zA-Z]+:[a-zA-Z]+:}","",thetext)
+
+    return thetext
+
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # aafmt
-The file book_chapters_LaTeX_original contains compilable LaTeX code for the text book.
-The file book_chapters_struc_LaTeX contains LaTeX formatted for an HTML conversion.
+The file book_chapters_LaTeX_original contains compilable LaTeX code for the textbook.
+For questions about the LaTeX please email Chris Thron at thron@tamuct.edu. For any bugs please submit an issue. 
+
+The file LaTeXtoLaTeX-master contains python scripts written by David Farmer to convert the original LaTeX code to 
+a structured LaTeX format. The output is used in a coversion to HTML for an online version of the textbook.
+
+To covert using LaTeXtoLaTex, make sure you have an appropriate version of python downloaded. If not you can download it
+here: www.python.org. To run the script you will open up a terminal or command prompt and type the follwoing:
+ltol.py inputfile otuputfile to convert one file at a time or 
+ltol.py inputdirectory outputdirectory to convert the contents of an entire directory. 
+If converting a directory, outputdirectory must already exist for the conversion to work. 
+
+
